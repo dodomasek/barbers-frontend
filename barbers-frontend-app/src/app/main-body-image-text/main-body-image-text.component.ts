88aa2d13743dc1379e8c44a6e50c0e2a326d1f51
dodo <dodo.masek@gmail.com>
2017-03-17 11:04:20 +0100

    css ---> scss main heading button added styles and structures started work on main heading button component

--- barbers-frontend-app/src/app/main-body-image-text/main-body-image-text.component.ts
+++ barbers-frontend-app/src/app/main-body-image-text/main-body-image-text.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-body-image-text',
   templateUrl: './main-body-image-text.component.html',
-  styleUrls: ['./main-body-image-text.component.css']
+  styleUrls: ['main-body-image-text.component.scss']
 })
 export class MainBodyImageTextComponent implements OnInit {
 
